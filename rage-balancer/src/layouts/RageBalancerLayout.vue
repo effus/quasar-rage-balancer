588e1fbc99dd37f5a58bce1cdff437656f0c9740
effusps@gmail.com <effusps@gmail.com>
2019-11-10 20:35:32 +0000

    my layout

---
+++ rage-balancer/src/layouts/RageBalancerLayout.vue
@@ -0,0 +1,28 @@
+<template>
+    <q-layout view="lHh Lpr lFf">
+        <q-header elevated>
+            <q-toolbar>
+                <q-btn
+                flat
+                dense
+                round
+                @click="leftDrawerOpen = !leftDrawerOpen"
+                icon="menu"
+                aria-label="Menu"
+                />
+
+                <q-toolbar-title>Rage Balancer</q-toolbar-title>
+            </q-toolbar>
+        </q-header>
+    </q-layout>
+</template>
+
+<script>
+export default { 
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
